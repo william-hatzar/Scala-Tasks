e88aff83b16e9e644275c9d7e1c057b86efdcae1
William Hatzar <williamhatzar15@hotmail.co.uk>
2019-01-09 11:38:19 +0000

    initial commit

--- src/main/scala/Wednesday_Exercise_1.scala
+++ src/main/scala/Wednesday_Exercise_1.scala
@@ -1,3 +1,22 @@
-object Wednesday_Exercise_1 {
+import scala.io.Source
+object Wednesday_Exercise_1 extends App {
+//  def starter(): Unit ={
+//    println("Input random integer")
+//    brokenKeyboard(scala.io.StdIn.readInt())
+//  }
+//
+//  def brokenKeyboard(numTimes:Int): Unit ={
+//   for(i <- 1 to numTimes){
+//
+//   }
+//  }
+
+
+
+//starter()
+
+ val donuts: Seq[String] = Seq("Plain Donut", "Strawberry Donut", "Glazed Donut")
+  println(s"Elements of donuts = ${donuts.last}")
+
 
 }
