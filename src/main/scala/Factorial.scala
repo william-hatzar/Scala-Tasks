336878a7d881a0a8fe34b2155ee49acb946ec8d9
William Hatzar <williamhatzar15@hotmail.co.uk>
2019-01-10 17:10:50 +0000

    Thursday Work

---
+++ src/main/scala/Factorial.scala
@@ -0,0 +1,19 @@
+object Factorial extends App {
+
+  def isFactorial(num1:Int): Unit ={
+    var x = 1
+    var factorial = 1;
+    while (factorial < num1){
+      x = x + 1
+      factorial = factorial * x;
+    }
+
+    if(factorial == num1) print(num1,x)
+    else println("NONE")
+
+  }
+
+  isFactorial(18)
+
+
+}
