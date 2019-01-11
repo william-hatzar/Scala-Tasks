30e2350f4c14f8b9e117e2d3db39f4650d378504
William Hatzar <williamhatzar15@hotmail.co.uk>
2019-01-11 15:58:18 +0000

    Trying to fix- but still problems

--- src/main/scala/Cypher.scala
+++ src/main/scala/Cypher.scala
@@ -2,26 +2,27 @@ object Cypher {
 
   def main (args: Array[String]): Unit =
   {
-    val allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     println("Enter text to convert to cipher")
-    val a = scala.io.StdIn.readLine()
+    var a = scala.io.StdIn.readLine()
     var plainText = a
     plainText = plainText.replaceAll(" ", "")
     plainText = plainText.toUpperCase
-    val len = plainText.length
-    val len1 = allChar.length
+    var len = plainText.length
+    var len1 = allChar.length
     var cipher = ""
     var i = 0
     var j = 0
 
     while(i < len){
-      val b = plainText.charAt(i)
+      var b = plainText.charAt(i)
       while(j < len1){
-        val c = allChar.charAt(j)
+        var c = allChar.charAt(j)
         if(c == b){
-          val index = allChar.indexOf(c)
-          val position = (len1-1)-index
-          cipher = cipher + allChar.charAt(position)
+          var index = allChar.indexOf(c)
+          var position = (len1-1)-index
+          //cipher = cipher + allChar.charAt(position)
+          cipher = cipher + allChar(position)
         }
         j = j + 1;
       }
