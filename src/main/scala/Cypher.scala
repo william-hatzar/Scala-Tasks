34d00235212e7683a8ae69d7dddb701723ee6c2a
William Hatzar <williamhatzar15@hotmail.co.uk>
2019-01-14 02:20:09 +0000

    Reassigning variables to val

--- src/main/scala/Cypher.scala
+++ src/main/scala/Cypher.scala
@@ -2,25 +2,25 @@ object Cypher {
 
   def main (args: Array[String]): Unit =
   {
-    var allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     println("Enter text to convert to cipher")
-    var a = scala.io.StdIn.readLine()
+    val a = scala.io.StdIn.readLine()
     var plainText = a
     plainText = plainText.replaceAll(" ", "")
     plainText = plainText.toUpperCase
-    var len = plainText.length
-    var len1 = allChar.length
+    val len = plainText.length
+    val len1 = allChar.length
     var cipher = ""
     var i = 0
     var j = 0
 
     while(i < len){
-      var b = plainText.charAt(i)
+      val b = plainText.charAt(i)
       while(j < len1){
-        var c = allChar.charAt(j)
+        val c = allChar.charAt(j)
         if(c == b){
-          var index = allChar.indexOf(c)
-          var position = (len1-1)-index
+          val index = allChar.indexOf(c)
+          val position = (len1-1)-index
           //cipher = cipher + allChar.charAt(position)
           cipher = cipher + allChar(position)
         }
